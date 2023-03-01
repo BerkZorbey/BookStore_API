9e83444585b2159fe107b642381324ba87626d2b
Taha Berk Yeşilalioğlu <berktaha1994@gmail.com>
2023-03-02 01:09:15 +0300

    fx author

---
+++ BookStoreTest/AuthorTest/Commands/Create/CreateAuthorTest.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using BookStore_API.BookStoreOperations.AuthorOperations.Commands.CreateAuthor;
+using BookStore_API.Models;
+using BookStore_API.StoreDbContext;
+using BookStoreTest.Moq;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BookStoreTest.AuthorTest.Commands.Create
+{
+    public class CreateAuthorTest : IClassFixture<CommonDI>
+    { 
+        private readonly BookStoreDbContext context;
+        private readonly IMapper mapper;
+
+        public CreateAuthorTest(CommonDI commonDI)
+        {
+            this.context = commonDI.Context;
+            this.mapper = commonDI.Mapper;
+        }
+
+        [Fact]
+        public void WhenAlreadyExitsAuthorFullNameIsGiven_InvalidOperationException_ShouldBeReturn()
+        {
+            // arrange (Hazırlık)
+            var author = new Author()
+            {
+                Name = "Charlotte Perkins",
+                Surname = "Gilman",
+                Birthday = new DateTime(1860, 07, 03)
+            };
+            context.Authors.Add(author);
+            context.SaveChanges();
+
+            CreateAuthorCommand command = new(context, mapper);
+            command.Model = new CreateAuthorCommand.CreateAuthorModel { Name = author.Name, Surname = author.Surname, Birthday = author.Birthday };
+
+            // act & assert (Çalıştırma - Doğrulama)
+            FluentActions
+                .Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Sequence contains more than one element");
+
+        }
+
+        [Fact]
+        public void WhenValidInputsAreGiven_Author_ShouldBeCreated()
+        {
+            // arrange
+            CreateAuthorCommand command = new(context, mapper);
+            CreateAuthorCommand.CreateAuthorModel model = new CreateAuthorCommand.CreateAuthorModel()
+            {
+                Name = "Taha Berk",
+                Surname = "Yeşilalioğlu",
+                Birthday = new DateTime(1999, 03, 28)
+            };
+
+            command.Model = model;
+
+            // act
+            FluentActions.Invoking(() => command.Handle()).Invoke();
+            // assert
+            var author = context.Authors.SingleOrDefault(g => g.Name == model.Name);
+            author.Should().NotBeNull();
+            author.Name.Should().Be(model.Name);
+            author.Surname.Should().Be(model.Surname);
+            author.Birthday.Should().Be(model.Birthday);
+        }
+    }
+}
---
+++ BookStoreTest/AuthorTest/Commands/Delete/DeleteAuthorTest.cs
@@ -0,0 +1,75 @@
+﻿using BookStore_API.BookStoreOperations.AuthorOperations.Commands.DeleteAuthor;
+using BookStore_API.Models;
+using BookStore_API.StoreDbContext;
+using BookStoreTest.Moq;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BookStoreTest.AuthorTest.Commands.Delete
+{
+    public class DeleteAuthorTest : IClassFixture<CommonDI>
+    {
+        private readonly BookStoreDbContext context;
+
+        public DeleteAuthorTest(CommonDI commonDI)
+        {
+            this.context = commonDI.Context;
+        }
+
+        [Fact]
+        public void WhenGivenAuthorIsNotFound_InvalidOperationException_ShouldBeReturn()
+        {
+            // arrange (Hazırlık)
+
+            DeleteAuthorCommand command = new(context);
+            command.AuthorId = 120;
+
+            // act & assert (Çalıştırma - Doğrulama)
+            FluentActions
+                .Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("The author to be deleted was not found");
+        }
+
+        [Fact]
+        public void WhenGivenAuthorHaveBook_InvalidOperationException_ShouldBeReturn()
+        {
+            // arrange (Hazırlık)
+
+            DeleteAuthorCommand command = new(context);
+            command.AuthorId = 1;
+
+            // act & assert (Çalıştırma - Doğrulama)
+            FluentActions
+                .Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Author have book(s) so you can't delete");
+        }
+
+        [Fact]
+        public void WhenValidInputsAreGiven_Author_ShouldBeDeleted()
+        {
+            // arrange
+            var newAuthor = new Author()
+            {
+                Name = "Taha Berk",
+                Surname = "Yeşilalioğlu",
+                Birthday = new DateTime(1999, 03, 28)
+            };
+            context.Authors.Add(newAuthor);
+            context.SaveChanges();
+
+            DeleteAuthorCommand command = new(context);
+            command.AuthorId = newAuthor.Id;
+
+            // act
+            FluentActions.Invoking(() => command.Handle()).Invoke();
+            // assert
+            var author = context.Authors.SingleOrDefault(a => a.Id == command.AuthorId);
+            author.Should().BeNull();
+        }
+    }
+}
